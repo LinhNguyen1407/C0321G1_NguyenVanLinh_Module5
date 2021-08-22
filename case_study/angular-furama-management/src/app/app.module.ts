7019282fbef570840ef2f69a43865bad1eeff44f
unknown <nvlinh.06dt3@gmail.com>
2021-08-22 09:38:41 +0700

    case study task 1 2

---
+++ case_study/angular-furama-management/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { UpdateCustomerComponent } from './customer/update-customer/update-customer.component';
+import { ListEmployeeComponent } from './employee/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { ListContractComponent } from './contract/list-contract/list-contract.component';
+import { CreateContractComponent } from './contract/create-contract/create-contract.component';
+import { CreateContractDetailComponent } from './contract-detail/create-contract-detail/create-contract-detail.component';
+import { ListServiceComponent } from './service/list-service/list-service.component';
+import { CreateServiceComponent } from './service/create-service/create-service.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListCustomerComponent,
+    CreateCustomerComponent,
+    UpdateCustomerComponent,
+    ListEmployeeComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    ListContractComponent,
+    CreateContractComponent,
+    CreateContractDetailComponent,
+    ListServiceComponent,
+    CreateServiceComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
